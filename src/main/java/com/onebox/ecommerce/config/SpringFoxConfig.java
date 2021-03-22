14727b8db8cfad39596a0a4a7a509be6ac1f8d62
Miguel Antunez <miguel.antunez@upcnet.es>
2021-03-22 17:49:32 +0100

    swagger configuration

---
+++ src/main/java/com/onebox/ecommerce/config/SpringFoxConfig.java
@@ -0,0 +1,30 @@
+package com.onebox.ecommerce.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SpringFoxConfig {
+
+    @Value("${swagger.host}")
+    private String swaggerHost;
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .host(swaggerHost)
+                .groupName("oneBox")
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+}
